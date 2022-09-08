d57727937259d741b492d09cfb99f0f652911a90
x1-windows10 <ijzone.dev@gmail.com>
2022-09-08 11:26:07 +0900

    AOP example with pure java

---
+++ features/src/main/java/spring/aop/ExamScore.java
@@ -0,0 +1,35 @@
+package spring.aop;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import spring.aop.entity.Exam;
+
+@AllArgsConstructor
+@Getter @Setter
+public class ExamScore implements Exam {
+
+	private int kor;
+	private int eng;
+	private int math;
+	private int com;
+	
+	@Override
+	public int total() {
+		int result = kor + eng + math + com;
+		return result;
+	}
+	
+	@Override
+	public float avg() {
+		float result = total() / 4.0f;
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "ExamScore [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+	}
+	
+	
+}
